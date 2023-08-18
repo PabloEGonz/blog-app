c1d58da03b8b346cc81967c991dd3340e2d318e0
Pablo <ezekielgonzalezr@gmail.com>
2023-08-17 21:55:55 -0600

    Correct linter offenses

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-    author = User.new(name: 'Rhaenyra', photo: 'url', bio: 'Queen')
-    subject { Post.new(author: author, title: 'Why I am the rightful heir', text: 'My father!')}
+  author = User.new(name: 'Rhaenyra', photo: 'url', bio: 'Queen')
+  subject { Post.new(author:, title: 'Why I am the rightful heir', text: 'My father!') }
 
-    before {subject.save}
-    
-    it 'Long title should be invalid' do
-        subject.title = 'a' * 260
-        expect(subject).to_not be_valid
-    end
-    it 'likes counter not an integer should be invalid' do
-        subject.likes_counter = 2.0
-        expect(subject).to_not be_valid
-    end
-    it 'comments counter not an integer should be invalid' do
-        subject.comments_counter = 6.0
-        expect(subject).to_not be_valid
-    end
-    it 'likes counter less than 0 should be invalid' do
-        subject.likes_counter = -9
-        expect(subject).to_not be_valid
-    end
-    it 'comments counterless than 0 should be invalid' do
-        subject.comments_counter = -10
-        expect(subject).to_not be_valid
-    end
+  before { subject.save }
+
+  it 'Long title should be invalid' do
+    subject.title = 'a' * 260
+    expect(subject).to_not be_valid
+  end
+  it 'likes counter not an integer should be invalid' do
+    subject.likes_counter = 2.0
+    expect(subject).to_not be_valid
+  end
+  it 'comments counter not an integer should be invalid' do
+    subject.comments_counter = 6.0
+    expect(subject).to_not be_valid
+  end
+  it 'likes counter less than 0 should be invalid' do
+    subject.likes_counter = -9
+    expect(subject).to_not be_valid
+  end
+  it 'comments counterless than 0 should be invalid' do
+    subject.comments_counter = -10
+    expect(subject).to_not be_valid
+  end
 end
