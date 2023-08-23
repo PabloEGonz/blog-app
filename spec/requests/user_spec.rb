b05d4b0bd7428799a0b3a0a44728a81b0b84295e
Pablo <ezekielgonzalezr@gmail.com>
2023-08-23 11:35:13 -0600

    Create tests for user controller

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -9,19 +9,42 @@ RSpec.describe 'user', type: :request do
         }
     }
 
-    let(:invalid_attributes) {
-        {
-        'name' => '',
-        'bio' => 'Hi!',
-        'photow' => 'url'
-        }
-    }
-
     describe "GET /index" do
         it 'renders a successful response' do
-            User.create! valid_attributes
             get users_url
             expect(response).to be_successful
+        end
+        
+        it 'checks http status' do 
+            get users_url
+            expect(response).to have_http_status(:success)
+        end
+        
+        it 'checks for index template' do
+            get users_url
+            assert_equal 'index', controller.action_name 
+        end
+        
+        it 'checks for index body content' do 
+            get users_url 
+            assert_match 'User name', response.body 
+        end
+    end
+
+    describe 'GET /show' do
+        it 'checks http status' do
+            get '/users/1'
+            expect(response).to have_http_status(:success)
+        end
+
+        it 'checks for show template' do
+            get '/users/1'
+            assert_equal 'show', controller.action_name 
+        end
+
+        it 'checks for show body content' do
+            get '/users/1'
+            assert_match 'Bio', response.body 
         end
     end
 end
