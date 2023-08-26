e678fc1f1e4a1c3b965d40057c1bb5ced5c6fe39
Pablo <ezekielgonzalezr@gmail.com>
2023-08-25 20:34:32 -0600

    Create comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    post = Post.find_by(id: params[:post_id])
+    new_comment = Comment.new(post: post, user: current_user)
+    new_comment.assign_attributes(params.require(:comment).permit(:text))
+    if new_comment.save
+        flash[:success] = 'The comment was added!'
+        redirect_to post_url(post)
+    else
+        flash[:error] = 'Could not post comment'
+        redirect_to post_url(post)
+    end
+  end
+end
